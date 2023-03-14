125dd9cc6591392f03b733c5c10a45f7313e1492
Malyadeep <22d0136@iitb.ac.in>
2023-03-14 17:09:38 +0530

    Equilibrium distribution
    Initial version of the module to calculate equilibrium distribution
    function. Currently supports first and second order in velocity.

---
+++ equilibrium.py
@@ -0,0 +1,27 @@
+import numpy as np
+import numba
+
+
+def secondOrder(f, Nx, Ny, w, c, u, v, rho, cs):
+    cs_2 = 1/(cs*cs)
+    cs_4 = cs_2/(cs*cs)
+    for j in range(Ny):
+        for i in range(Nx):
+            t2 = u[i, j]*u[i, j] + v[i, j]*v[i, j]
+            for k in range(w.shape[0]):
+                t1 = c[0, k]*u[i, j] + c[1, k]*v[i, j]
+                f[i, j, k] = w[k]*rho[i, j]*(1 + t1*cs_2 + 0.5*t1*t1*cs_4 -
+                                             0.5*t2*cs_2)
+
+
+def firstOrder(f, Nx, Ny, w, c, u, v, rho, cs):
+    cs_2 = 1/(cs*cs)
+    for j in range(Ny):
+        for i in range(Nx):
+            for k in range(w.shape[0]):
+                t1 = c[0, k]*u[i, j] + c[1, k]*v[i, j]
+                f[i, j, k] = w[k]*rho[i, j]*(1 + t1*cs_2)
+
+
+if __name__ == '__main__':
+    print('equilibrium distribution function')
