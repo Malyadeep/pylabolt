5211bdd999a09bbed447ea212d73c5ce1d2ecd91
Malyadeep <22d0136@iitb.ac.in>
2023-03-19 09:01:33 +0530

    > Collision and Streaming happen in different step
    To avoid overwriting of populations collision and streaming
    are now divided into two steps. A new character array called nodeType
    denotes the type of node, solid, liquid or boundary.
    Also 2 sets of population are maintained to facilitate setting up
    boundary conditions.

--- collisionStreaming.py
+++ collisionStreaming.py
@@ -13,17 +13,25 @@ class collisionStreaming:
         self.f_new = np.copy(f)
 
     @numba.njit
-    def BGK(self, f, f_eq, deltaT, deltaX, c):
+    def BGK(self, f, f_new, f_eq, nodeType):
         for i in range(f.shape[0]):
             for j in range(f.shape[1]):
-                for k in range(f.shape[2]):
-                    i_new = (i + int(self.c[k, 0]*self.dtdx)
-                             + f.shape[0]) % f.shape[0]
-                    j_new = (j + int(self.c[k, 1]*self.dtdx)
-                             + f.shape[1]) % f.shape[1]
-                    self.f_new[i_new, j_new, k] = f[i, j, k] + self.deltaT * \
-                        self.omega * (f_eq[i, j, k] - f[i, j, k])
-        f[:] = self.f_new
+                if nodeType[i, j] != 's':
+                    for k in range(f.shape[2]):
+                        f[i, j, k] = f_new[i, j, k] + self.deltaT * \
+                            self.omega * (f_eq[i, j, k] - f[i, j, k])
+
+    @numba.njit
+    def stream(self, f, f_new, nodeType):
+        for i in range(f.shape[0]):
+            for j in range(f.shape[1]):
+                if nodeType[i, j] != 's':
+                    for k in range(1, f.shape[2]):
+                        i_old = (i - int(self.c[k, 0]*self.dtdx)
+                                 + f.shape[0]) % f.shape[0]
+                        j_old = (j - int(self.c[k, 1]*self.dtdx)
+                                 + f.shape[1]) % f.shape[1]
+                        f_new[i, j, k] = f[i_old, j_old, k]
 
 
 if __name__ == '__main__':
