0954aa9d72db492bc9d83badeaca4f49786f1150
Malyadeep <22d0136@iitb.ac.in>
2023-03-23 11:01:06 +0530

    collisionModels modified.
    > They now iterate over distributions local to elements

--- collisionModels.py
+++ collisionModels.py
@@ -2,10 +2,7 @@ import numba
 
 
 @numba.njit
-def BGK(f, f_new, f_eq, nodeType, preFactor):
-    for i in range(f.shape[0]):
-        for j in range(f.shape[1]):
-            if nodeType[i, j] != 's':
-                for k in range(f.shape[2]):
-                    f[i, j, k] = f_new[i, j, k] + preFactor * \
-                        (f_eq[i, j, k] - f[i, j, k])
+def BGK(f, f_new, f_eq, preFactor):
+    for k in range(f.shape[0]):
+        f[k] = f_new[k] + preFactor * \
+            (f_eq[k] - f[k])
