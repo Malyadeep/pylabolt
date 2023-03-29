6c34c48a619cdd60928f130099f767d6b03f2bd6
mithun-m-nair <mithunmnair1@gmail.com>
2023-03-29 22:10:00 +0530

    Added noOfDirections to lattice class

--- lattice.py
+++ lattice.py
@@ -3,12 +3,13 @@ import numba
 
 spec = [
     ('c', numba.int64[:, :]),
-    ('invList', numba.int64[:]),
+    ('invList', numba.int64[:])
     ('cs', numba.float64),
     ('latticeType', numba.types.string),
     ('w', numba.float64[:]),
     ('deltaX', numba.float64),
     ('deltaT', numba.float64),
+    ('noOfDirections', numba.int64)
 ]
 
 
@@ -27,11 +28,13 @@ class lattice:
                                1/36, 1/36, 1/36, 1/36])
             self.invList = np.array([0, 3, 4, 1, 2, 7, 8, 5, 6],
                                     dtype=np.int64)
+            self.noOfDirections = 9
 
         elif lat == 'D1Q3':
             self.c = np.array([[0, 0], [1, 0], [-1, 0]], dtype=np.int64)
             self.w = np.array([2/3, 1/6, 1/6])
             self.invList = np.array([0, 2, 1], dtype=np.int64)
+            self.noOfDirections = 3
 
 
 def createLattice(latticeDict):
