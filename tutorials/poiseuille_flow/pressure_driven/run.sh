4ee3b5605002b9dfc4ed9e69c209684c195c78ca
Malyadeep <22d0136@iitb.ac.in>
2023-07-14 19:38:33 +0530

    Minor changes to tutorials

--- tutorials/poiseuille_flow/pressure_driven/run.sh
+++ tutorials/poiseuille_flow/pressure_driven/run.sh
@@ -6,7 +6,7 @@ mpirun -np 3 pylabolt --reconstruct time -t 7251
 mv output/7251/fields.dat ./fields_incompressible.dat
 
 sed -i "s/incompressible/secondOrder/g" simulation.py
-mpirun -np 3 pylabolt --solver fluidLB > log_compressible.txt 2> log_incompressible.txt
+mpirun -np 3 pylabolt --solver fluidLB > log_compressible.txt 2> err_compressible.txt
 mpirun -np 3 pylabolt --reconstruct time -t 17013
 mv output/17013/fields.dat ./fields_compressible.dat
 
