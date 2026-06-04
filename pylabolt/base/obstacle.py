99e2e7de5f44e8b269387ba68f0b03e12f196354
Malyadeep <malyadeep1999@gmail.com>
2026-06-05 01:28:04 +0530

    add: obstacle type circle and ellipse - validation and definition

--- pylabolt/base/obstacle.py
+++ pylabolt/base/obstacle.py
@@ -183,13 +183,21 @@ class Obstacle:
         obstacle_type = user_obstacle_dict["type"]
 
         if obstacle_type == "circle":
-            obstacle = create_circle()
+            obstacle = Circle(
+                user_obstacle_name,
+                user_obstacle_dict,
+                control
+            )
         elif obstacle_type == "ellipse":
-            obstacle = create_ellipse()
+            obstacle = Ellipse(
+                user_obstacle_name,
+                user_obstacle_dict,
+                control
+            )
         elif obstacle_type == "custom":
             obstacle = create_custom_obstacle()
         else:
-            raise ValueError("Unsupported obstacle type")
+            raise ValueError("Unsupported obstacle type: " + obstacle_type)
 
         self.obstacles.append(obstacle)
 
@@ -255,12 +263,267 @@ class Obstacle:
         print_log("\n", domain.mpi_rank, verbose)
 
 
-def create_circle():
-    pass
+class Circle:
+    def __init__(
+        self,
+        user_obstacle_name,
+        user_obstacle_dict,
+        control
+    ):
+        """
+        Container object for obstacle type: circle
+        Attributes:
+
+        """
+        self.name = user_obstacle_name
+        self.type = "circle"
+
+        if "radius" not in user_obstacle_dict:
+            raise ValueError(
+                "radius missing for obstacle: " + user_obstacle_name
+            )
+        radius = user_obstacle_dict["radius"]
+        if not isinstance(radius, (float, int)):
+            raise ValueError(
+                "radius must be a float or int for obstacle type circle: " +
+                user_obstacle_name
+            )
+        self.radius = control.precision(radius)
+
+        if "center" not in user_obstacle_dict:
+            raise ValueError(
+                "center missing for obstacle: " + user_obstacle_name
+            )
+        center = user_obstacle_dict["center"]
+        if not isinstance(center, list):
+            raise ValueError(
+                "center must be a list for obstacle type circle: " +
+                user_obstacle_name
+            )
+        self.center = np.array(center, dtype=control.precision)
+
+        self.inclination_angle = control.precision(0)
+
+        if "density" not in user_obstacle_dict:
+            raise ValueError(
+                "density missing for obstacle: " + user_obstacle_name
+            )
+        density = user_obstacle_dict["density"]
+        if not isinstance(density, (float, int)):
+            raise ValueError(
+                "density must be a float or int for obstacle type circle: " +
+                user_obstacle_name
+            )
+        self.density = control.precision(density)
+
+        if "static" not in user_obstacle_dict:
+            raise ValueError(
+                "static missing for obstacle: " + user_obstacle_name
+            )
+        static = user_obstacle_dict["static"]
+        if not isinstance(static, (bool, np.bool_)):
+            raise ValueError(
+                "static must be True/False for obstacle type circle: " +
+                user_obstacle_name
+            )
+        self.static = static
+
+        if self.static:
+            self.motion_type = None
+            self.degree_of_freedom = None
+            self.linear_velocity = np.zeros(2, dtype=control.precision)
+            self.angular_velocity = control.precision(0)
+        else:
+            if "solid_motion_dict" not in user_obstacle_dict:
+                raise ValueError(
+                    "solid_motion_dict missing in obstacle: " +
+                    user_obstacle_name
+                )
+            solid_motion_dict = user_obstacle_dict["solid_motion_dict"]
+            for key in [
+                "type",
+                "degree_of_freedom",
+                "linear_velocity",
+                "angular_velocity"
+            ]:
+                if key not in solid_motion_dict:
+                    raise ValueError(
+                        key + " missing in solid_motion_dict: " +
+                        user_obstacle_name
+                    )
+            self.motion_type = solid_motion_dict["type"]
+            if self.motion_type not in ["fixed_velocity", "calculated"]:
+                raise ValueError(
+                    "Unsupported motion type: " + self.motion_type +
+                    " in obstacle: " + user_obstacle_name
+                )
+            self.degree_of_freedom = solid_motion_dict["degree_of_freedom"]
+            if self.motion_type not in ["rotation", "translation", "both"]:
+                raise ValueError(
+                    "Unsupported degree of freedom: " + self.motion_type +
+                    " in obstacle: " + user_obstacle_name
+                )
+            self.linear_velocity = solid_motion_dict["linear_velocity"]
+            if not isinstance(self.linear_velocity, list):
+                raise ValueError(
+                    "linear_velocity must be a list [ux, uy]: " +
+                    user_obstacle_name
+                )
+            self.linear_velocity = np.array(
+                self.linear_velocity, dtype=control.precision
+            )
+            self.angular_velocity = solid_motion_dict["angular_velocity"]
+            if not isinstance(self.angular_velocity, (int, float)):
+                raise ValueError(
+                    "angular_velocity must be a int/float: " +
+                    user_obstacle_name
+                )
+            self.angular_velocity = np.array(
+                self.angular_velocity, dtype=control.precision
+            )
 
 
-def create_ellipse():
-    pass
+class Ellipse:
+    def __init__(
+        self,
+        user_obstacle_name,
+        user_obstacle_dict,
+        control
+    ):
+        """
+        Container object for obstacle type: circle
+        Attributes:
+
+        """
+        self.name = user_obstacle_name
+        self.type = "ellipse"
+
+        if "semi_major_axis" not in user_obstacle_dict:
+            raise ValueError(
+                "semi_major_axis missing for obstacle: " + user_obstacle_name
+            )
+        semi_major_axis = user_obstacle_dict["semi_major_axis"]
+        if not isinstance(semi_major_axis, (float, int)):
+            raise ValueError(
+                "semi_major_axis must be a float or int for obstacle" +
+                " type ellipse: " + user_obstacle_name
+            )
+        self.semi_major_axis = control.precision(semi_major_axis)
+
+        if "semi_minor_axis" not in user_obstacle_dict:
+            raise ValueError(
+                "semi_minor_axis missing for obstacle: " + user_obstacle_name
+            )
+        semi_minor_axis = user_obstacle_dict["semi_minor_axis"]
+        if not isinstance(semi_minor_axis, (float, int)):
+            raise ValueError(
+                "semi_minor_axis must be a float or int for obstacle" +
+                " type ellipse: " + user_obstacle_name
+            )
+        self.semi_minor_axis = control.precision(semi_minor_axis)
+
+        if "inclination_angle" not in user_obstacle_dict:
+            raise ValueError(
+                "inclination_angle missing for obstacle: " + user_obstacle_name
+            )
+        inclination_angle = user_obstacle_dict["inclination_angle"]
+        if not isinstance(semi_minor_axis, (float, int)):
+            raise ValueError(
+                "inclination_angle must be a float or int for obstacle" +
+                " type ellipse representing angle in degrees: " +
+                user_obstacle_name
+            )
+        self.inclination_angle = control.precision(inclination_angle)
+
+        if "center" not in user_obstacle_dict:
+            raise ValueError(
+                "center missing for obstacle: " + user_obstacle_name
+            )
+        center = user_obstacle_dict["center"]
+        if not isinstance(center, list):
+            raise ValueError(
+                "center must be a list for obstacle type circle: " +
+                user_obstacle_name
+            )
+        self.center = np.array(center, dtype=control.precision)
+
+        if "density" not in user_obstacle_dict:
+            raise ValueError(
+                "density missing for obstacle: " + user_obstacle_name
+            )
+        density = user_obstacle_dict["density"]
+        if not isinstance(density, (float, int)):
+            raise ValueError(
+                "density must be a float or int for obstacle type circle: " +
+                user_obstacle_name
+            )
+        self.density = control.precision(density)
+
+        if "static" not in user_obstacle_dict:
+            raise ValueError(
+                "static missing for obstacle: " + user_obstacle_name
+            )
+        static = user_obstacle_dict["static"]
+        if not isinstance(static, (bool, np.bool_)):
+            raise ValueError(
+                "static must be True/False for obstacle type circle: " +
+                user_obstacle_name
+            )
+        self.static = static
+
+        if self.static:
+            self.motion_type = None
+            self.degree_of_freedom = None
+            self.linear_velocity = np.zeros(2, dtype=control.precision)
+            self.angular_velocity = control.precision(0)
+        else:
+            if "solid_motion_dict" not in user_obstacle_dict:
+                raise ValueError(
+                    "solid_motion_dict missing in obstacle: " +
+                    user_obstacle_name
+                )
+            solid_motion_dict = user_obstacle_dict["solid_motion_dict"]
+            for key in [
+                "type",
+                "degree_of_freedom",
+                "linear_velocity",
+                "angular_velocity"
+            ]:
+                if key not in solid_motion_dict:
+                    raise ValueError(
+                        key + " missing in solid_motion_dict: " +
+                        user_obstacle_name
+                    )
+            self.motion_type = solid_motion_dict["type"]
+            if self.motion_type not in ["fixed_velocity", "calculated"]:
+                raise ValueError(
+                    "Unsupported motion type: " + self.motion_type +
+                    " in obstacle: " + user_obstacle_name
+                )
+            self.degree_of_freedom = solid_motion_dict["degree_of_freedom"]
+            if self.motion_type not in ["rotation", "translation", "both"]:
+                raise ValueError(
+                    "Unsupported degree of freedom: " + self.motion_type +
+                    " in obstacle: " + user_obstacle_name
+                )
+            self.linear_velocity = solid_motion_dict["linear_velocity"]
+            if not isinstance(self.linear_velocity, list):
+                raise ValueError(
+                    "linear_velocity must be a list [ux, uy]: " +
+                    user_obstacle_name
+                )
+            self.linear_velocity = np.array(
+                self.linear_velocity, dtype=control.precision
+            )
+            self.angular_velocity = solid_motion_dict["angular_velocity"]
+            if not isinstance(self.angular_velocity, (int, float)):
+                raise ValueError(
+                    "angular_velocity must be a int/float: " +
+                    user_obstacle_name
+                )
+            self.angular_velocity = np.array(
+                self.angular_velocity, dtype=control.precision
+            )
 
 
 def create_custom_obstacle():
