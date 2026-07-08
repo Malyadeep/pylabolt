196bf62a0b032e4a00be85df819e0c42c076a42e
Malyadeep <malyadeep1999@gmail.com>
2026-07-08 22:18:32 +0530

    equilibrium_kernels for CPU
    1. Compute equilibrium distribution at given node
    2. Also contains, initialization kernels for
    distributions

--- pylabolt/parallel/cpu/collision_kernels.py
+++ pylabolt/parallel/cpu/collision_kernels.py
@@ -40,7 +40,8 @@ def BGK_density_based_second_order_None(
             for k in range(no_of_directions):
                 cu = cx[k] * velocity_local_x + cy[k] * velocity_local_y
                 pop_eq = weights[k] * density_local * (
-                    1 + inv_cs_2 * cu + 0.5 * inv_cs_4 * cu * cu - 0.5 * inv_cs_2 * u2
+                    1 + inv_cs_2 * cu + 0.5 * inv_cs_4 * cu * cu -
+                    0.5 * inv_cs_2 * u2
                 )
                 pop[ind, k] = (1 - omega) * pop_new[ind, k] + omega * pop_eq
 
@@ -84,7 +85,8 @@ def BGK_density_based_second_order_guo_linear(
             for k in range(no_of_directions):
                 cu = cx[k] * velocity_local_x + cy[k] * velocity_local_y
                 pop_eq = weights * density * (
-                    1 + inv_cs_2 * cu + 0.5 * inv_cs_4 * cu * cu - 0.5 * inv_cs_2 * u2
+                    1 + inv_cs_2 * cu + 0.5 * inv_cs_4 * cu * cu -
+                    0.5 * inv_cs_2 * u2
                 )
                 force_term = weights[k] * (
                     cx[k] * force_local_x + cy[k] * force_local_y
@@ -135,10 +137,13 @@ def BGK_density_based_second_order_guo_second_order(
             for k in range(no_of_directions):
                 cu = cx[k] * velocity_local_x + cy[k] * velocity_local_y
                 pop_eq = weights * density * (
-                    1 + inv_cs_2 * cu + 0.5 * inv_cs_4 * cu * cu - 0.5 * inv_cs_2 * u2
+                    1 + inv_cs_2 * cu + 0.5 * inv_cs_4 * cu * cu -
+                    0.5 * inv_cs_2 * u2
                 )
-                const_x = (cx[k] - velocity_local_x) * inv_cs_2 + cu * cx[k] * inv_cs_4
-                const_y = (cy[k] - velocity_local_y) * inv_cs_2 + cu * cy[k] * inv_cs_4
+                const_x = (cx[k] - velocity_local_x) * inv_cs_2 +\
+                    cu * cx[k] * inv_cs_4
+                const_y = (cy[k] - velocity_local_y) * inv_cs_2 +\
+                    cu * cy[k] * inv_cs_4
                 force_term = weights[k] * (
                     const_x * force_local_x + const_y * force_local_y
                 )
